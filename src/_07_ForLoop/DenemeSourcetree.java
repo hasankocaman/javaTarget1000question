5223c543de28dbc8e0313b12b860f461285076f5
Hasan Kocaman <hasank4311@gmail.com>
2022-12-16 10:11:49 +0300

    16araliklocalde

--- src/_07_ForLoop/DenemeSourcetree.java
+++ src/_07_ForLoop/DenemeSourcetree.java
@@ -6,6 +6,7 @@ public class DenemeSourcetree {
         System.out.println("sourcetree");
         System.out.println("pull request nasil yapilir");
         System.out.println("degisikligi web sitesinde yaptim");
+        System.out.println("degisikligi localimde yaptim");
         
     }
 }
