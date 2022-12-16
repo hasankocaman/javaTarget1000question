1814450a41b58e6396eb1a3340a7359bf276e025
hasankocaman <94443851+hasankocaman@users.noreply.github.com>
2022-12-16 09:56:48 +0300

    degisikligiwebdeyaptim
    webde degisiklik yaptim master branchte

--- src/_07_ForLoop/DenemeSourcetree.java
+++ src/_07_ForLoop/DenemeSourcetree.java
@@ -5,5 +5,7 @@ public class DenemeSourcetree {
     public static void main(String[] args) {
         System.out.println("sourcetree");
         System.out.println("pull request nasil yapilir");
+        System.out.println("degisikligi web sitesinde yaptim");
+        
     }
 }
