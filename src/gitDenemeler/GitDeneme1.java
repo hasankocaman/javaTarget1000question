b1aabf1699e7c83dfe12503921fe982a3366c0a7
hasankocaman <94443851+hasankocaman@users.noreply.github.com>
2022-12-16 11:51:05 +0300

    web sitesinde eklenen satir
    web sitesinde eklenen satir no 6

--- src/gitDenemeler/GitDeneme1.java
+++ src/gitDenemeler/GitDeneme1.java
@@ -3,5 +3,6 @@ package gitDenemeler;
 public class GitDeneme1 {
     public static void main(String[] args) {
         System.out.println("deneme1");
+        System.out.println("web sitesinde eklenen satir");
     }
 }
